33c2320639d8746daede923b119842f33492ae1c
Aspandiyar-Zhakyp <75847712+Aspandiyar-Zhakyp@users.noreply.github.com>
2021-02-12 16:55:55 +0600

    DZ4/ Practice

---
+++ ClassIntroductionPractice/Money.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassIntroductionPractice
+{
+    public class Money
+    {
+        int nominal;       
+        int quantityNotes; 
+        int totalCash = 0;
+
+        public Money()
+        {
+            nominal = 0;
+            quantityNotes = 0;
+        }
+
+        public Money(int nominal, int quantityNotes)
+        {
+            this.nominal = nominal;
+            this.quantityNotes = quantityNotes;
+        }
+
+        public override string ToString()
+        {
+            return $"У Вас в кармане {quantityNotes} куп. номиналом {nominal} тнг.";
+        }
+
+        public string CheckAvailability(double costOfGoods)
+        {
+            totalCash = nominal * quantityNotes;   
+            if (costOfGoods < totalCash)
+            {
+                return $"У Вас достаточно наличных ({totalCash} тнг.) для покупки товара стоимость {costOfGoods} тнг.";
+            }
+            else
+            {
+                return $"К сожалению, для покупки товара стоимостью {costOfGoods} Вам не хватает {costOfGoods - totalCash} тнг. ";
+            }
+        }
+
+        public string CalculationOfQuantity(double costOfGoods)
+        {
+            int quantityGoods = (int)(totalCash / costOfGoods);
+            return $"На имеющиеся {totalCash} тнг. можно купить {quantityGoods} ед. товара стоимостью {costOfGoods} грн.";
+        }
+
+        public int Nominal
+        {
+            get => nominal;
+            set => nominal = value;
+        }
+
+        public int QuantityNotes
+        {
+            get => quantityNotes;
+            set => quantityNotes = value;
+        }
+
+        public int TotalCash
+        {
+            get => totalCash;
+        }
+    }
+}
---
+++ ClassIntroductionPractice/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ClassIntroductionPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Money purse = new Money(1000, 15);                         
+            Console.WriteLine(purse);
+            Console.WriteLine(purse.CheckAvailability(251));        
+            Console.WriteLine(purse.CalculationOfQuantity(1000));   
+
+            Console.ReadKey();
+        }
+    }
+}
